fc16d4feede3d1e129bee97ed0289a0e9415b47f
Yaodong Zhao <y@whir.org>
2016-02-08 16:44:41 -0700

    Put library data into instance variables

--- lib/ulysses/library.rb
+++ lib/ulysses/library.rb
@@ -1,14 +1,25 @@
 module Ulysses
   class Library
 
+    DEFAULT_LIBRARY_DIR = '~/Library/Mobile Documents/X5AZV975AG~com~soulmen~ulysses3/Documents/Library'
+
     attr_reader :dirname
 
     def initialize(dirname = nil)
-      dirname ||= '~/Library/Mobile Documents/X5AZV975AG~com~soulmen~ulysses3/Documents/Library'
-      @dirname = File.expand_path(dirname)
+      @dirname = File.expand_path(dirname ||= DEFAULT_LIBRARY_DIR)
     end
 
     def groups
+      @groups ||= parse_groups
+    end
+
+    def reload
+      @groups = nil
+    end
+
+    private
+
+    def parse_groups
       Dir.glob(File.join @dirname, 'Groups-ulgroup', '*.ulgroup').map do |info_file|
         Group.new info_file
       end
