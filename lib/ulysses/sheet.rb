e71b43caee482d402dc484a9f9fc99fe47336c7f
Yaodong Zhao <y@whir.org>
2016-02-12 16:19:24 -0700

    Add sheet#markup

--- lib/ulysses/sheet.rb
+++ lib/ulysses/sheet.rb
@@ -7,8 +7,12 @@ module Ulysses
       @dirname = dirname
     end
 
+    def markup
+      @markup ||= parse_markup
+    end
+
     def xml
-      @xml ||= File.read(File.join(@dirname, 'Content.xml'))
+      @xml ||=  Nokogiri::XML(File.read(File.join(@dirname, 'Content.xml')))
     end
 
     def to_html
@@ -16,7 +20,26 @@ module Ulysses
     end
 
     def reload
-      @xml, @html = nil
+      @markup, @xml, @html = nil
+    end
+
+    private
+
+    def parse_xml_attributes(node)
+      Hash[node.attributes.map { |nm, el| [nm.to_sym, el.value] }]
+    end
+
+    def parse_markup
+      segment = xml.xpath('/sheet/markup')[0]
+      markup  = parse_xml_attributes(segment)
+      markup[:definitions] = begin
+        defines = segment.children.select { |node| node.element? }.map do |node|
+          attrs = parse_xml_attributes(node)
+          [attrs[:definition].to_sym, attrs]
+        end
+        Hash[defines]
+      end
+      markup
     end
 
   end
