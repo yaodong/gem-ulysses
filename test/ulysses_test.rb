6e6086a63fd573c0a536c7716fa5a04a6b1b4fdd
Yaodong Zhao <y@whir.org>
2016-02-13 11:32:49 -0700

    Add print test

--- test/ulysses_test.rb
+++ test/ulysses_test.rb
@@ -4,4 +4,13 @@ class UlyssesTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Ulysses::VERSION
   end
+
+  def test_print
+    library_path = File.expand_path(File.join(__dir__, 'fixtures', 'library'))
+    export_file  = File.expand_path(File.join(__dir__, 'fixtures', 'print.html'))
+    library = Ulysses::Library.new library_path
+    printer = Ulysses::Printer.new(library)
+    assert_equal File.read(export_file), printer.print
+  end
+
 end
